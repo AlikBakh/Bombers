4163abe0c1b84ae18e867e7dc6b7049b0052e8d9
Alik <baxishev-alik@mail.ru>
2021-11-28 04:01:34 +0300

    изменил главное меню и добавил кнопку выхода

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,13 +1,9 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    private int num;
-    public void TaskOnClick()
-    {
-        num = (gameObject.name == "menu" || gameObject.name == "menuBtn") ? 0 : 1;
-        SceneManager.LoadScene(num);
-    }
+    public void LoadScene() => SceneManager.LoadScene(gameObject.name == "menu" || gameObject.name == "menuBtn" ? 0 : 1);
+    
+    public void CloseApp() => Application.Quit();
 }

94f2e31fe7931e4e4d809b7703f2f9e06d16dcc7
Alik <baxishev-alik@mail.ru>
2021-11-28 05:38:35 +0300

    Добавил класс для логов

---
+++ Assets/Scripts/LogFiles.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class LogFiles
+{
+    public static void WriteToFile(string Message, string msgType)
+    {
+        string path = $"{AppDomain.CurrentDomain.BaseDirectory}\\Logs";
+        if (!Directory.Exists(path))
+            Directory.CreateDirectory(path);
+
+        string filepath = $"{AppDomain.CurrentDomain.BaseDirectory}\\Logs\\ServiceLog_{DateTime.Now.Date.ToShortDateString().Replace('/', '-')}.txt";
+        if (!File.Exists(filepath))
+            using (StreamWriter sw = File.CreateText(filepath))
+                sw.WriteLine($"{DateTime.Now} + [{msgType}]: {Message}");
+        else
+            using (StreamWriter sw = File.AppendText(filepath))
+                sw.WriteLine($"{DateTime.Now} + [{msgType}]: {Message}");
+    }
+}
